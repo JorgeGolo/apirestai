8d527ddc89e7db6dd226f0cfb8174e38862f2154
Jorge Gómez López <jorgelogomez@gmail.com>
2024-10-16 09:35:13 +0200

    Redesigning the wholoe thing with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Añadir los archivos de Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
